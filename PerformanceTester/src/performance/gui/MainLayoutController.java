b62df1058d0544018c171ed07a27baca398d00d3
joharteaga <john.arteaga@gmail.com>
2017-07-13 03:28:23 -0500

    initial gui

---
+++ PerformanceTester/src/performance/gui/MainLayoutController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package performance.gui;
+
+
+import javafx.fxml.FXML;
+
+/**
+ * FXML Controller class
+ *
+ * @author ru5h
+ */
+public class MainLayoutController {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    public void initialize() {
+        // TODO
+    }    
+    
+}
